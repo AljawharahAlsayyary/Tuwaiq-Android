9fcb858c5dd9c6c7fc92486661452ce09902249e
AljawharahAlsayyary <121759359+AljawharahAlsayyary@users.noreply.github.com>
2023-01-02 19:36:39 +0300

    Add files via upload

---
+++ Exerecise 1.kt
@@ -0,0 +1,13 @@
+package com.example.twaiqandroid1
+
+fun main() {
+
+    val s: String = "Abdullah"
+    val a: Float = 20.6f
+    val b: Double = 8.9746325
+    val c: Byte = 25
+    val d: Short = 2022
+    val e: Long = 321984526954
+    val f: Boolean = true
+    val g: Char = 'a'
+}
