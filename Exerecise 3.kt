2d0296ce3389fd5de75782b233a63ec19e9d348d
AljawharahAlsayyary <121759359+AljawharahAlsayyary@users.noreply.github.com>
2023-01-03 13:37:18 +0300

    Add files via upload

---
+++ Exerecise 3.kt
@@ -0,0 +1,11 @@
+package com.example.twaiqandroid1
+
+fun main(){
+
+  var i = 100
+    for (i in 0..100) {
+        println(i)
+        if (i==71)break
+    }
+    println("IT'S OVER 70!!!")
+}
---
+++ Exerecise 4.kt
@@ -0,0 +1,15 @@
+package com.example.twaiqandroid1
+
+fun main() {
+    var t = 35
+    while (t >= 10) {
+        println(t)
+        t -= 1
+        when{
+            (t == 30)  -> println("it's Hot")
+            (t == 20) -> println("it's Comfy")
+            (t == 15) -> println("IT'S COLD!!!")
+        }
+
+    }
+}
