25e541a5126e87474dbe5c4a6db98bea096f5b51
AljawharahAlsayyary <121759359+AljawharahAlsayyary@users.noreply.github.com>
2023-01-03 16:09:41 +0300

    Add files via upload

--- Exerecise 4.kt
+++ Exerecise 4.kt
@@ -2,13 +2,13 @@ package com.example.twaiqandroid1
 
 fun main() {
     var t = 35
-    while (t >= 10) {
-        println(t)
+    while (t > 10) {
         t -= 1
         when{
             (t == 30)  -> println("it's Hot")
             (t == 20) -> println("it's Comfy")
             (t == 15) -> println("IT'S COLD!!!")
+            else -> println(t)
         }
 
     }
