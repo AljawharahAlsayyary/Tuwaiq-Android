349caf3f5ba8cbd8456ec13e42e4b7786dd59e15
AljawharahAlsayyary <121759359+AljawharahAlsayyary@users.noreply.github.com>
2023-01-08 14:59:20 +0300

    Add files via upload
    the homework after correction

--- Exerecise 5.kt
+++ Exerecise 5.kt
@@ -1,15 +1,31 @@
 package com.example.twaiqandroid1
 
+import kotlin.random.Random
+
 fun main(){
 
-    var numbers = arrayListOf(10.2,11.5,17.4,12.3,19.7)
-    var sum = 0.0
+    var numbers = arrayListOf<Double>()
 
-    for(num in numbers){
-        sum += num
-    }
-    var average = sum / numbers.size
+    var counter = 1.0
+    for (i in 1..5) numbers.add(++counter)
 
-    println(average)
+    // solution 1:
+    var average = 0.0
+    for (i in numbers ) average += i
+    println(average / numbers.size)
+
+    // solution 2:
+    println(numbers.average())
+
+
+    /* var numbers = arrayListOf(10.2,11.5,17.4,12.3,19.7)
+     var sum = 0.0
+
+     for(num in numbers){
+         sum += num
+     }
+     var average = sum / numbers.size
+
+     println(average) */
 
 }
